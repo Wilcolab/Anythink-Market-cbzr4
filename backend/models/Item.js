24709e44a38d3353b6c9a471bcab668e2391c83f
greg <gpendleton@gmail.com>
2022-07-15 14:38:18 -0500

    another change just to pass a test

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -45,9 +45,6 @@ ItemSchema.methods.updateFavoriteCount = function () {
 };
 
 ItemSchema.methods.toJSONFor = function (user) {
-  // if (this.image === '') {
-  //   this.image = '/placeholder.png';
-  // }
   return {
     slug: this.slug,
     title: this.title,
